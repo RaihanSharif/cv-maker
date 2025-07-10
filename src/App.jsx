a38a10b6b9b60f8fe72fd3b4d9394f06d336876e
Raihan Sharif <raihansharif92@gmail.com>
2025-07-10 17:41:00 +0100

    EducationEdit generates a list of buttons based on the educationList

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./styles/App.css";
 import Sidebar from "./components/Sidebar";
 import PersonalInfoEdit from "./components/PersonalInfoEdit";
+import { EducationListEdit } from "./components/EducationEdit";
 
 // const dummyPersonalData = {
 //   name: "Raihan Sharif",
@@ -10,9 +11,41 @@ import PersonalInfoEdit from "./components/PersonalInfoEdit";
 //   website: "raihan.com",
 // };
 
+const dummyEduData = [
+  {
+    id: 0,
+    school: "King's college",
+    course: "computer science",
+    grade: "diploma",
+    start: new Date("2016-09"),
+    end: new Date("2020-06"),
+    description: "did not complete the course",
+  },
+  {
+    id: 1,
+    school: "UCL",
+    course: "History",
+    grade: "First",
+    start: new Date("2020-09"),
+    end: new Date("2023-06"),
+    description: "excellente!",
+  },
+  {
+    id: 2,
+    school: "Imperial",
+    course: "War Studies",
+    grade: "second",
+    start: new Date("2018-09"),
+    end: new Date("2021-06"),
+    description: "Ayy caramba!",
+  },
+];
+
 function App() {
+  // const testArr = ["kings", "UCL", "Oxford"];
   const [personalData, setPersonalData] = useState("");
 
+  // passed to PersonalInfoEdit component
   function handlePersonalOnChange(e) {
     const { name, value } = e.target;
     setPersonalData((prev) => ({ ...prev, [name]: value }));
@@ -21,6 +54,8 @@ function App() {
   return (
     <>
       <Sidebar personalData={personalData} onChange={handlePersonalOnChange} />
+      <EducationListEdit data={dummyEduData} />
+
       <div style={{ display: "flex", gap: "1rem", padding: "10px" }}>
         <p>Name: {personalData.name}</p>
         <p>Email: {personalData.email}</p>
--- src/components/EducationEdit.jsx
+++ src/components/EducationEdit.jsx
@@ -2,3 +2,74 @@
 // info:
 //      school name, course, start and end dates of study
 // has a check box to say if currently in the course.
+
+// render the list of education buttons
+
+function EducationListEdit({ data }) {
+  console.log(data);
+  return (
+    <section className="education-btns">
+      <ul>
+        {data.map((item) => {
+          return (
+            <li key={item.id}>
+              <button>{item.course}</button>
+            </li>
+          );
+        })}
+      </ul>
+    </section>
+  );
+}
+
+// function EducationItemEdit({ educationItem }) {
+//   return (
+//     <section>
+//       <form className="education-edit-form">
+//         <label htmlFor="school">
+//           School/University name:{" "}
+//           <input
+//             type="text"
+//             name="school"
+//             id="school"
+//             autoComplete="organization"
+//           />
+//         </label>
+//         <label htmlFor="course">
+//           Course:{" "}
+//           <input type="text" name="course" id="course" autoComplete="none" />
+//         </label>
+
+//         <label htmlFor="grade">
+//           Grade:{" "}
+//           <input type="text" name="course" id="course" autoComplete="none" />
+//         </label>
+
+//         <label htmlFor="start-date">
+//           Start date:{" "}
+//           <input
+//             type="month"
+//             name="start-date"
+//             id="start-date"
+//             autoComplete="month"
+//           />
+//         </label>
+//         <label htmlFor="end-date">
+//           End date:{" "}
+//           <input
+//             type="month"
+//             name="end-date"
+//             id="end-date"
+//             autoComplete="month"
+//           />
+//         </label>
+//         <label htmlFor="description">
+//           Start date:{" "}
+//           <textarea name="description" id="description" autoComplete="none" />
+//         </label>
+//       </form>
+//     </section>
+//   );
+// }
+
+export { EducationListEdit };
